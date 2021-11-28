eed325cc13917e1b3bc651141f11cd0ecef63935
osKlonowski <osklonowski@gmail.com>
2021-11-28 15:48:46 +0100

    completed basic home page components

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,21 @@
-import { View, Text, Button, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/core";
 import useAuth from "../hooks/useAuth";
+import tw from "tailwind-rn";
+import { Ionicons } from "@expo/vector-icons";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const { logout, user } = useAuth();
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -15,11 +25,57 @@ const HomeScreen = () => {
   return (
     <SafeAreaView>
       {/* Header */}
-      <View></View>
+      <View style={tw("items-center relative")}>
+        <TouchableOpacity style={tw("absolute left-5 top-3")} onPress={logout}>
+          <Image
+            style={tw("h-10 w-10 rounded-full")}
+            source={{ uri: user.photoURL }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={tw("h-14 w-14 ")} source={require("../logo.png")} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={tw("absolute right-5 top-3")}
+          onPress={() => navigation.navigate("Chat")}
+        >
+          <Ionicons name="chatbubbles-sharp" size={30} color="black" />
+        </TouchableOpacity>
+      </View>
       {/* End of Header */}
-      <Text>I am the home screen</Text>
-      <Button title="Go to Chat" onPress={() => navigation.navigate("Chat")} />
-      <Button title="Sign Out" onPress={logout} />
+      {/* Home Swipe Stack Card */}
+      <View>
+        <Image
+          style={tw("m-2 self-center rounded-2xl w-11/12 h-5/6")}
+          source={{
+            uri: "https://www.kolpaper.com/wp-content/uploads/2020/11/Minimal-Mobile-Wallpaper.jpg",
+          }}
+        />
+        <Text
+          style={tw("absolute bottom-32 left-10 text-white font-bold text-xl")}
+        >
+          Oskar Klonowski
+        </Text>
+      </View>
+      {/* End of Home Swipe Stack Card */}
+      {/* Bottom Controls */}
+      <View style={tw("flex flex-row justify-evenly ")}>
+        <TouchableOpacity
+          style={tw(
+            "items-center justify-center rounded-full w-16 h-16 bg-red-200"
+          )}
+        >
+          <Ionicons name="chatbubbles-sharp" size={30} color="red" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={tw(
+            "items-center justify-center rounded-full w-16 h-16 bg-green-200"
+          )}
+        >
+          <Ionicons name="chatbubbles-sharp" size={30} color="green" />
+        </TouchableOpacity>
+      </View>
+      {/* End of Bottom Controls */}
     </SafeAreaView>
   );
 };
