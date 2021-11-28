3c6c5833e350229acf24ab5dfced12980e21cd96
osKlonowski <osklonowski@gmail.com>
2021-11-28 15:06:27 +0100

    finished login screen + building header in home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,26 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
+import { View, Text, Button, SafeAreaView } from "react-native";
+import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/core";
 import useAuth from "../hooks/useAuth";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <View>
+    <SafeAreaView>
+      {/* Header */}
+      <View></View>
+      {/* End of Header */}
       <Text>I am the home screen</Text>
       <Button title="Go to Chat" onPress={() => navigation.navigate("Chat")} />
       <Button title="Sign Out" onPress={logout} />
-    </View>
+    </SafeAreaView>
   );
 };
 
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,44 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import { useNavigation } from "@react-navigation/core";
+import React, { useLayoutEffect } from "react";
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import useAuth from "../hooks/useAuth";
+import tw from "tailwind-rn";
 
 const LoginScreen = () => {
-  const { signInWithGoogle } = useAuth();
+  const { signInWithGoogle, loading } = useAuth();
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
   return (
-    <View>
-      <Text>Please Login to the App</Text>
-      <Button title="Login" onPress={signInWithGoogle} />
+    <View style={tw("flex-1")}>
+      <ImageBackground
+        resizeMode="cover"
+        style={tw("flex-1")}
+        source={{
+          uri: "https://www.kolpaper.com/wp-content/uploads/2020/11/Minimal-Mobile-Wallpaper.jpg",
+        }}
+      >
+        <TouchableOpacity
+          style={[
+            tw("absolute bottom-40 w-52 bg-white p-4 rounded-2xl"),
+            { marginHorizontal: "25%" },
+          ]}
+          onPress={signInWithGoogle}
+        >
+          <Text style={tw("font-semibold text-center")}>
+            {loading ? "Loading..." : "Sign in with Google"}
+          </Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
